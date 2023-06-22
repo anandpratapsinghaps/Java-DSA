40e915f7512a53359cc08103532ee8a546a584d6
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-06-22 16:56:45 +0530

    last commit

--- Basics/sum.java
+++ Basics/sum.java
@@ -1,8 +1,7 @@
 import java.util.*;
-import java.util.*;
 class sum{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         int Sum=a+b;
