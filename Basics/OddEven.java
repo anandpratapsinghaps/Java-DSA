810169f38289085543891b644ab54ab803afd548
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-06-26 00:03:54 +0530

    last commit

--- Basics/OddEven.java
+++ Basics/OddEven.java
@@ -5,10 +5,8 @@ public class OddEven{
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        int n;
         System.out.println("Enter the size of the array: ");
-        n = sc.nextInt();
+        int n = sc.nextInt();
 
         int arr[] = new int[n];
         System.out.println("Enter the array elements: ");
