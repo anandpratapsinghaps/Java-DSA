7b58f7bc5da5ea12ac7bef3c0f391254b935aa01
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-06-27 23:21:48 +0530

    last commit

---
+++ Basics/Loops/prime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your number:");
+        int n=sc.nextInt();
+        if(n==2){
+            System.out.println("n is Prime");
+        } else{
+        boolean isPrime = true;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(i%10==0){
+                isPrime = false;
+            }
+        }
+        if(isPrime == true){
+            System.out.println("n is Prime");
+        } else{
+            System.out.println("n is not Prime");
+        }
+        }
+        
+    }
+}
