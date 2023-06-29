7c064a25ae594fd971f8ad9407900a33ecee9612
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-06-29 18:05:02 +0530

    last commit

---
+++ Basics/Pattern/number.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class number {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
