f2a602777893b513d91adf608857fb02b2ff0f57
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-06-29 19:11:28 +0530

    last commit

---
+++ Basics/Pattern/ch.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class ch {
+    public static void main(String[] args) {
+        char c='A';
+        int n=6;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(c);
+                c++;
+            }
+            System.out.println();
+
+        }
+        
+    }
+}
