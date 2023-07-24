3b4b4b8dcccc3bfb158ba3036bdea61d9b9ef643
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-24 23:40:31 +0530

    last commit

--- Basics/Pattern/ch.java
+++ Basics/Pattern/ch.java
@@ -4,7 +4,7 @@ public class ch {
     public static void main(String[] args) {
         char c='A';
         int n=6;
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(c);
                 c++;
