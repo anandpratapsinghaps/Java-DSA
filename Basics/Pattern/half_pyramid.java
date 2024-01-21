df89d2a5bca5f6c03d7983a582fe7ae6a3c4a182
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-01-21 22:57:36 +0530

    last commit

---
+++ Basics/Pattern/half_pyramid.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+public class half_pyramid {
+    public static void pym(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pym(6);
+    }
+}
