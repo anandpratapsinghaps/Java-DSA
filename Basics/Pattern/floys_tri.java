73bb58c0c2698ca7ecafe5b63bcba0847af6dce6
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-01-22 00:13:11 +0530

    last commit

---
+++ Basics/Pattern/floys_tri.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class floys_tri {
+    public static void floydtriangle(int n){
+        int counter=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(counter+ "  ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        floydtriangle(5);
+    }
+}
