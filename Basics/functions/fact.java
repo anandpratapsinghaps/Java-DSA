cb4ea9068a47f61411936c021536d601a6d88774
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-01-21 12:29:57 +0530

    last commit

--- Basics/functions/fact.java
+++ Basics/functions/fact.java
@@ -8,7 +8,15 @@ public class fact {
         }
         return f;
     }
+    public static int bincoff(int n, int r){
+        int fact_n=factorial(n);
+        int fact_r=factorial(r);
+        int fact_nmr=factorial(n-r);
+
+        int bincoff=fact_n/(fact_r*fact_nmr);
+        return bincoff;
+    }
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(bincoff(5, 2));
     }
 }
