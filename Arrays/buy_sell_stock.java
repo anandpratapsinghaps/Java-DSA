5d2f4f92e0b2acac18e1b03da8f37c751de69d80
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-02-01 20:10:51 +0530

    last commit

---
+++ Arrays/buy_sell_stock.java
@@ -0,0 +1,18 @@
+public class buy_sell_stock {
+    public static int buysellstock(int arr[]){
+        int buyprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<buyprice)
+                buyprice=arr[i];
+                int profit=arr[i]-buyprice;
+                maxprofit=Math.max(profit,maxprofit);
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int arr[]={7,1,5,3,6,4};
+        System.out.println(buysellstock(arr));
+    }
+}
+
