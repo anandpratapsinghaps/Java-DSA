eb05167d16cf8c8d4ffc729f5ba5e8c1fd8b1f2a
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-02-11 14:28:43 +0530

    last commit

---
+++ Arrays/Sorting_Algo/bubblesort.java
@@ -0,0 +1,27 @@
+package Sorting_Algo;
+
+public class bubblesort {
+    public static void BubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,1,5,6,3,9};
+        BubbleSort(arr);
+        printarr(arr);
+    }
+}
