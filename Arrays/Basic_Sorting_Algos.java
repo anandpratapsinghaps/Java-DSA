0eaf4c920ae33c84080c6a10ff1065f185ed37af
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-03-10 00:35:15 +0530

    last commit

--- Arrays/Basic_Sorting_Algos.java
+++ Arrays/Basic_Sorting_Algos.java
@@ -43,6 +43,30 @@ public class Basic_Sorting_Algos{
             arr[prev+1]=curr;
         }
     }
+
+//counting sort
+
+    public static void countingsort(int arr[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest,arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void printarr(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
@@ -50,10 +74,11 @@ public class Basic_Sorting_Algos{
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[]={2,3,8,1,5,6,7,9,4};
+        int arr[]={2,2,2,2,3,8,1,5,6,7,9,4};
         // BubbleSort(arr);
         // Selectionsort(arr);
-        insertionsort(arr);
+        // insertionsort(arr);
+        countingsort(arr);
         printarr(arr);
     }
 }
