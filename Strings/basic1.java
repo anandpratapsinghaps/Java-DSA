8bc103fba5d09b17d46fa7e5ea1dfa86780d40e3
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-04-30 00:55:08 +0530

    last commit

--- Strings/basic1.java
+++ Strings/basic1.java
@@ -2,11 +2,22 @@ package Strings;
 import java.util.*;
 
 public class basic1 {
+    public static void printletter(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String name;
+        // Scanner sc=new Scanner(System.in);
+        // String name;
         // name=sc.next();
-        name=sc.nextLine();
-        System.out.println(name);
+        // name=sc.nextLine();
+        // System.out.println(name);
+        String firstname = "Anand";
+        String lastname = "Singh";
+        String fullname= firstname+" "+lastname;
+        // System.out.println(fullname.length());
+        printletter(fullname);
     }
 }
