f40d414b4075e0385575ddbd1f06032018eb7cfb
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-06-16 16:58:03 +0530

    last commit

--- Recursion/basic.java
+++ Recursion/basic.java
@@ -11,10 +11,20 @@ public class basic {
         System.out.println(n+" ");
         printDec(n-1);
     }
+
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fact1=fact(n-1);
+        int fn=n*fact(n-1);
+        return fn;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter your number");
+        System.out.print("Enter your number: ");
         int n=sc.nextInt();
-        printDec(n);
+        // printDec(n);5
+        System.out.println(fact(n));
     }
 }
