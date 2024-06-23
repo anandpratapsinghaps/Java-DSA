c701f41d960a0a207dd1eaa4a7410bf04fc4cfe8
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-06-23 23:34:55 +0530

    last commit

--- Recursion/practice.java
+++ Recursion/practice.java
@@ -12,13 +12,24 @@ public class practice {
         }
         findOcc(arr, key, i+1);
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
+
+    static String digits[] = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+    public static void printDigits(int number){
+        if(number==0){
+            return;
         }
-        findOcc(arr, 5, 0);
+        int lastDigit=number%10;
+        printDigits(number/10);
+        System.out.print(digits[lastDigit]+" ");
+    }
+    public static void main(String[] args){
+        // Scanner sc=new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int arr[] = new int[n];
+        // for(int i=0; i<n; i++){
+        //     arr[i]=sc.nextInt();
+        // }
+        // findOcc(arr, 5, 0);
+        printDigits(1234);
     }
 }
