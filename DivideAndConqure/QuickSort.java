16325c04254887049314e9ce71c8696f90ad8ea7
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-06-27 15:39:58 +0530

    last commit

--- DivideAndConqure/QuickSort.java
+++ DivideAndConqure/QuickSort.java
@@ -1,4 +1,56 @@
 package DivideAndConqure;
+
+import java.util.Scanner;
+
 public class QuickSort {
     
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int arr[] ,int si, int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int pIdx=partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1);
+        quickSort(arr, pIdx+1, ei);
+    }
+
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i=si-1;
+
+        for(int j=si; j<ei; j++){
+            if(arr[j]<=pivot){
+                i++;
+
+                //swap
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        for(int idx=0; idx<n; idx++){
+            arr[idx]=sc.nextInt();
+        }
+        quickSort(arr, 0, arr.length-1);
+        printArr(arr);
+        sc.close();
+    }
 }
