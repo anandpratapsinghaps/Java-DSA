e8bf33581e3c570d1d11252299b8d0238ddf2918
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-07-04 17:19:00 +0530

    last commit

---
+++ Backtracking/NQueens.java
@@ -0,0 +1,38 @@
+package Backtracking;
+
+public class NQueens {
+
+    public static void nqueens(char board[][], int row){
+        //base case
+        if(row==board.length){
+            printQueen(board);
+            return;
+        }
+
+        for(int j=0; j<board.length; j++){
+            board[row][j]='Q';
+            nqueens(board, row+1);
+            board[row][j]='.';
+        }
+    }
+
+    public static void printQueen(char board[][]){
+        System.out.println("-----chess board------");
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=2;
+        char board[][]=new char[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j]='.';
+            }
+        }
+        nqueens(board,0);
+    }
+}
