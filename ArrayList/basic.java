de701ed7d12ba3359425abb71c8774e491f9f32d
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-07-11 13:50:01 +0530

    last commit

--- ArrayList/basic.java
+++ ArrayList/basic.java
@@ -5,13 +5,15 @@ public class basic {
         ArrayList<Integer> list=new ArrayList<>();
 
         //add element
-        list.add(1);
         list.add(2);
+        list.add(5);
+        list.add(7);
+        list.add(8);
         list.add(3);
 
         //add element at particular index
-        list.add(1,20);
-        System.out.println(list);
+        // list.add(1,20);
+        // System.out.println(list);
 
     //     //get element
     //     int element=list.get(2);
@@ -30,11 +32,25 @@ public class basic {
     // System.out.println(list.contains(6));
     
 
-    System.out.println(list.size());   //.size() is method not a property like .length in array
+    // System.out.println(list.size());   //.size() is method not a property like .length in array
 
+    // for(int i=0; i<list.size(); i++){
+    //     System.out.print(list.get(i)+" ");
+    // }
+
+    //print reverse
+    // for(int i=list.size()-1; i>=0; i--){
+    //     System.out.print(list.get(i)+" ");
+    // }
+    
+    //find max
+    int max=Integer.MIN_VALUE;
     for(int i=0; i<list.size(); i++){
-        System.out.print(list.get(i)+" ");
+        if(max<list.get(i)){
+            max=list.get(i);
+        }
     }
+    System.out.println(max);
 
     }
 }
