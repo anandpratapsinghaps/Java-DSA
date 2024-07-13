3976216dbb34c932554986b74e7ddc9c24bff9e9
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-07-13 16:04:20 +0530

    last commit

--- ArrayList/Pairsum.java
+++ ArrayList/Pairsum.java
@@ -30,14 +30,43 @@ public class Pairsum {
         }
         return false;
     }
+
+    public static boolean pairSum2(ArrayList<Integer> list, int target){
+        //calculate breaking point
+        int bp=-1;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i)>list.get(i+1)){
+                bp = i;
+                break;
+            }
+        }
+        int lp = bp+1;
+        int rp = bp;
+        int n=list.size();
+
+        while(lp!=rp){
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+            if(list.get(lp)+list.get(rp)<target){
+                lp=(lp+1)%n;
+            }
+            else{
+                rp=(n+rp-1)%n;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         ArrayList<Integer> list =new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        int target = 5;
-        System.out.println(pairSum(list, target));
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16;
+        // System.out.println(pairSum(list, target));
+        System.out.println(pairSum2(list, target));
     }
 }
