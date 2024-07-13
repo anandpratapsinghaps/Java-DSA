36fee9c9b25a13714893a6809b042c33fd5e6e8a
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-07-13 16:59:23 +0530

    last commit

---
+++ LinkedList/basic.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class basic {
+
+    public static class Node{
+        int data;
+        Node node;
+
+        public Node(int data){
+            this.data=data;
+            this.node=null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static void main(String[] args) {
+        basic ll = new basic();
+    }
+}
