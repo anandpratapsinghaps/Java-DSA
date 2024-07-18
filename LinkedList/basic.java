138006e9b38416731893367fd3f82d7abdcac880
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-07-18 22:55:19 +0530

    last commit

--- LinkedList/basic.java
+++ LinkedList/basic.java
@@ -4,18 +4,36 @@ public class basic {
 
     public static class Node{
         int data;
-        Node node;
-        
-
+        Node next;
+    
         //constructor
         public Node(int data){
             this.data=data;
-            this.node=null;
+            this.next=null;
         }
     }
 
     public static Node head;
     public static Node tail;
+
+    //no need to make method static isko hum ll obj k through call krenge aur main method bhi static hai
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        
+    }
     public static void main(String[] args) {
+        basic ll = new basic();
+        ll.addFirst(1);
+        ll.addFirst(2);
     }
 }
