bc2bbabb022d5a0c739967e5632d351fe05fc782
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-07-20 14:43:03 +0530

    last commit

--- LinkedList/basic.java
+++ LinkedList/basic.java
@@ -86,6 +86,27 @@ public class basic {
         size--;
         return val;
     }
+
+    public int removeLast(){
+        if(size==0){
+            System.out.println("khaali hai");
+            return Integer.MIN_VALUE;
+        } else if(size==1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
     public static void main(String[] args) {
         basic ll = new basic();
         ll.addFirst(1);
@@ -98,5 +119,7 @@ public class basic {
         ll.remvoeFirst();
         ll.printLL();
         System.out.println(ll.size);
+        ll.removeLast();
+        ll.printLL();
     }
 }
