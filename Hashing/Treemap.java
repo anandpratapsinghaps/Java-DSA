b699eb4aa1a73cb398c9453c2d4d649fa665b245
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-08-21 23:13:21 +0530

    last commit

---
+++ Hashing/Treemap.java
@@ -0,0 +1,14 @@
+package Hashing;
+import java.util.TreeMap;
+public class Treemap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+        map.put("c", 1);
+        map.put("a", 2);
+        map.put("d", 3);
+        map.put("f", 4);
+        map.put("g", 5);
+        System.out.println(map);
+    }
+}
+
