957ac86197e6f04898c800c8a8837febc74d68ae
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-08-25 00:46:22 +0530

    last commit

---
+++ Hashing/MajorityElement.java
@@ -0,0 +1,10 @@
+package Hashing;
+import java.util.*;
+public class MajorityElement {
+    public static int majorityelement(int[] arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+    }
+    public static void main(String[] args) {
+        int[] arr = {}
+    }
+}
