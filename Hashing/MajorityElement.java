07c086f50e02ecff0169a0a7c50cffbf67cafddf
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-08-25 01:08:20 +0530

    last commit

--- Hashing/MajorityElement.java
+++ Hashing/MajorityElement.java
@@ -1,10 +1,23 @@
 package Hashing;
 import java.util.*;
 public class MajorityElement {
-    public static int majorityelement(int[] arr){
-        HashMap<Integer, Integer> map = new HashMap<>();
-    }
     public static void main(String[] args) {
-        int[] arr = {}
+        int[] arr = {1,3,2,5,1,3,5,1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<arr.length; i++){
+            // if(map.containsKey(arr[i])){
+            //     map.put(arr[i], map.get(arr[i])+1);
+            // } else{
+            //     map.put(arr[i], 1);
+            // }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for(Integer key : map.keySet()){
+            if(map.get(key)>arr.length/3){
+                System.out.println(key);
+            }
+        }
     }
 }
