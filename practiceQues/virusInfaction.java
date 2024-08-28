78cda562b52dfa55d16ed194edfc5682d8478e6b
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-08-28 17:42:18 +0530

    last commit

--- practiceQues/virusInfaction.java
+++ practiceQues/virusInfaction.java
@@ -5,18 +5,18 @@ public class virusInfaction {
         String s = "00100100";
         int k = 1;
         int n = s.length();
-        boolean[] marked = new boolean[n];
+        StringBuilder sb = new StringBuilder("0".repeat(n));
 
         for(int i=0; i<n; i++){
             if(s.charAt(i) == '1'){
                 for(int j=Math.min(0,i-k); j<Math.min(n-1,i+k); j++){
-                    marked[j] = true;
+                    sb.setCharAt(j, '1');
                 }
             }
         }
         int count = 0;
         for(int i=0; i<n; i++){
-            if(marked[i]){
+            if(sb.charAt(i) == '1'){
                 count++;
             }
         }
