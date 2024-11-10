81155d7b5ad5481ce620516c2f712ae95e3ed24e
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-11-10 19:41:14 +0530

    last commit

---
+++ OOPs/Oops.java
@@ -0,0 +1,36 @@
+package OOPs;
+
+public class Oops {
+    
+}
+class Pen{
+
+    //properties
+
+    String color;
+    int tip;
+
+    //function
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+class Student{
+
+    //properties
+
+    String name;
+    int age;
+    int percentage;
+
+    //function
+
+    void calcPercentage (int m1, int m2, int m3){
+        percentage = (m1 + m1 + m3) / 3;
+    }
+}
