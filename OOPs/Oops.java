d7aca4b33b3d0e0aa9388a9e376cb18f0d869665
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-11-13 00:49:42 +0530

    last commit

--- OOPs/Oops.java
+++ OOPs/Oops.java
@@ -2,11 +2,15 @@ package OOPs;
 
 public class Oops {
     public static void main(String[] args) {
-        Pen p1 = new Pen();  //created an object p1 Pen() -> constructor
-        p1.setColor("Red");
-        p1.setTip(5);
-        System.out.println(p1.getColor());
-        System.out.println(p1.getTip());
+        // Pen p1 = new Pen();  //created an object p1 Pen() -> constructor
+        // p1.setColor("Red");
+        // p1.setTip(5);
+        // System.out.println(p1.getColor());
+        // System.out.println(p1.getTip());
+        Student s1 = new Student();
+        Student s2 = new Student("Anand");
+        Student s3 = new Student(143);
+
     }
 }
 class Pen{
@@ -43,7 +47,22 @@ class Student{
 
     //function
 
-    void calcPercentage (int m1, int m2, int m3){
-        percentage = (m1 + m1 + m3) / 3;
+    // void calcPercentage (int m1, int m2, int m3){
+    //     percentage = (m1 + m1 + m3) / 3;
+    // }
+
+    //constructors
+
+    Student(){            //non parameterized
+        System.out.println("Hello");
+    }
+
+
+    //parameterized 
+    Student(String name){
+        this.name = name;
+    }
+    Student(int age){
+        this.age = age;
     }
 }
